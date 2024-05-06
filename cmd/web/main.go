6d512ef258d52fc1ac9f0f957362da2c6dc58659
Patryk Ptak <ptaszqq@gmail.com>
2024-05-06 21:53:53 +0100

    postgres

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,29 +1,57 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 type application struct {
 	logger *slog.Logger
+	db     *sql.DB
 }
 
 func main() {
+	connectionString := flag.String("db", "", "postgresql connection string")
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
+	db, err := openDB(*connectionString)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
 	app := &application{
 		logger: logger,
+		db:     db,
 	}
 
 	logger.Info("starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
