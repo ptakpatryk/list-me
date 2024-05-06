80268ebd34fa74e53dd5dff6f581bf37139bb8c1
Patryk Ptak <ptaszqq@gmail.com>
2024-05-06 15:57:55 +0100

    basic templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
+	"text/template"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from list-me!"))
+  files := []string{
+    "./ui/html/base.tmpl.html",
+    "./ui/html/partials/nav.tmpl.html",
+    "./ui/html/pages/home.tmpl.html",
+  }
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+  err = ts.ExecuteTemplate(w, "base", nil)
+  if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+  }
 }
 
 func listView(w http.ResponseWriter, r *http.Request) {
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,10 +7,17 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+  //Static server
+  fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+  //Handlers
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /list/view/{id}", listView)
 	mux.HandleFunc("GET /list/create", listCreate)
 	mux.HandleFunc("POST /list/create", listCreatePost)
+
 
 	log.Print("starting server on :4000")
 
