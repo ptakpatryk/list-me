589c03bfb781940532e65f3055c50ebf7b39309a
Patryk Ptak <ptaszqq@gmail.com>
2024-06-08 08:22:32 +0100

    e2e server tests

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -15,6 +15,8 @@ func (app *application) routes() http.Handler {
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
+  mux.HandleFunc("GET /ping", ping)
+
 	//Handlers
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
 	mux.Handle("GET /list/view/{id}", dynamic.ThenFunc(app.listView))
